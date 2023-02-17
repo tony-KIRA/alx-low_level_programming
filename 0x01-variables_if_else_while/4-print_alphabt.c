9e488f103abf09d774f881ffee97ff5446667cc3
tony-KIRA <migwianthony6@gmail.com>
2023-02-17 15:57:55 -0800

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 
+/**
+ * main - print alphabet expect q and e
+ * Return: zero
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+
+
+		{
+			putchar(ch);
+		}
+
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
