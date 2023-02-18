1d820f3ab8effc9a5f07b8225b40d6a252d73b94
tony-KIRA <migwianthony6@gmail.com>
2023-02-18 03:59:36 -0800

    do more of whats required

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints 3 digit 
+ * Result: zero
+ */
+
+int main(void)
+{
+	int a, b, z;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (z = 50; z < 58; z++)
+			{
+				if (z > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(z);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+
