99a77ea9baf3f9b650b3cfc7359ae52d8d38d9a6
tony-KIRA <migwianthony6@gmail.com>
2023-02-20 14:26:25 -0800

    gtateful

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/** main - prints digits of base 10
+/**
+ * main - prints digits of base 10
  * Return: zero 
  */
 
 int main(void)
 {
 	int i;
+	
 
 	for (i = 0; i <= 9; i++)
 		printf("%d", i);
