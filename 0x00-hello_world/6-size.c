461419cfd5598ff7826d8af55b59d132b450c863
tony-KIRA <migwianthony6@gmail.com>
2023-02-20 13:36:30 -0800

    champ

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	long long int d;
 	float f;
 
-
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
