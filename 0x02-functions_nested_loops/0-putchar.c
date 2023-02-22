9a7fbe90315ddc84ffcc75c7fe117e63afacf816
tony-KIRA <migwianthony6@gmail.com>
2023-02-22 08:15:54 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * return: zero
+ */
+
+int main(void)
+{
+	char h []= "_putchar";
+	int i;
+
+	for (i = 0; h[i] != '\0'; i++)
+	{
+		_putchar(h[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
