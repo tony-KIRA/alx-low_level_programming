85eb88a2e0cbda907ddd4d49e7cb5f58c8ca770e
tony-KIRA <migwianthony6@gmail.com>
2023-02-28 11:38:38 -0800

    arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,24 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-
-int main(void)
-{
-	int n;
-	int a[5];
-	int *p;
-
-
-	a[2] = 1024;
-	p = &n;
-	/*
-	 *  write your line of code here...
-	 *  Remember:
-	 *  - you are not allowed to use a
-	 *  - you are not allowed to modify p
-	 *  - only one statement
-	 *  - you are not allowed to code anything else than this line of code
-	 */
-	;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
-}
+#include <string.h>
+char _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s)
+#endif /* MAIN_H */
